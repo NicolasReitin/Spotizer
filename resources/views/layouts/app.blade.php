6c1ee0fd02d3f76a7af63bb3f4eeb06fc134f868
Nicolas REITIN <reitin.pro@gmail.com>
2022-10-11 10:46:12 +0200

    update error js no-read on albums.show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,11 @@
                         <a href="{{ url('/') }}"><img class="logo ms-5" src="{{ asset('assets/images/logo.png') }}" alt="logo spotizer"></a> 
                         <ul>
                             <li><a href="{{ route('titres.index') }}">Titres</a></li>
-                            <li><a href="">Playlist</a></li>
                             <li><a href="{{ route('groupes.index') }}">Groupes</a></li>
                             <li><a href="{{ route('albums.index') }}">Albums</a></li>
                             <li><a href="{{ route('artistes.index') }}">Artistes</a></li>
                             <li><a href="{{ route('genres.index') }}">Genres</a></li> 
+                            <li><a href="">Playlist</a></li>
                         </ul>
                     </div>
 
